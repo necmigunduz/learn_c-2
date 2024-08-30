beffce529876041a62d3a0187b9025898638dd76
Necmi Gunduz <necmigunduz@gmail.com>
2024-08-30 13:38:38 +0300

    add: learn c# 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll total is: {roll1} + {roll2} + {roll3} = {total} and ...");
+
+if (total > 14) {
+    Console.WriteLine("You won!");
+} else {
+    Console.WriteLine("You lost!");
+}
